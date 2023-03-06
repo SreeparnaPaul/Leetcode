459c993633ca1d29b122b7ff79aa9c744793c4ec
Sreeparna Paul <sreeparna.paul@indusnet.co.in>
2023-03-06 12:49:08 +0530

    first commit

---
+++ java/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int[] result = new int[]{i, j};
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] nums = new int[]{2, 5, 6, 7, 8, 9, 1};
+        int target = 11;
+        int[] result = twoSum.twoSum(nums, target);
+        if (result != null) {
+            System.out.println("Array of indexes of nums,whose sum is target: " + Arrays.toString(result));
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
